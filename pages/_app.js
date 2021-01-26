6cfa406757527271bac258fba5cb30b717f2a3bf
Siphosenkosi Ndhlovu <sipho.ndhlovu@outlook.com>
2021-01-26 07:30:07 +0200

    Added theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
-
+import { ThemeProvider } from 'emotion-theming'
+import theme from '@rebass/preset'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
