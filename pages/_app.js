328be21b5a9af8c7bdb9d6f1e87cc51a09954aa2
Siphosenkosi Ndhlovu <sipho.ndhlovu@outlook.com>
2021-01-29 14:52:28 +0200

    Added theme config

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
-import '../styles/globals.css'
-import { ThemeProvider } from 'emotion-theming'
-import theme from '@rebass/preset'
+import { ThemeProvider } from 'theme-ui'
+import theme from '../styles/theme'
+theme.styles.root = {
+  fontFamily: 'Futura, sans-serif'
+}
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
---
+++ styles/theme.js
@@ -0,0 +1,28 @@
+export default {
+  breakpoints: ['768px', '1024px', '1400px'],
+  fontSizes: ['14px', '15px', '16px', '18px', '20px', '22px', '25px', '30px', '35px', '50px', '100px'],
+  fonts: {
+    body: 'Futura, sans-serif',
+    heading: 'Chapaza, serif',
+  },
+  fontWeights: {
+    body: 500,
+    heading: 400,
+  },
+  colors: {
+    blue: '#3359CF',
+    altBlue: '#302EE6',
+    lightBlue: '#3359CF26',
+    darkBlue: '#142F82',
+    purple: '#5E2CDB',
+    dark: '#1D1D1B',
+    light: '#E3E3E3',
+    background: 'white'
+  },
+  text: 'dark',
+  background: 'white',
+  space: [0, 5, 15, 20, 25, 30, 35, 50, 80, 100],
+  sizes: [52, 80],
+  
+  styles: {}
+};
