69edef2b83c23e4a9c3f01ef5d4eb6ebbe1852a8
Siphosenkosi Ndhlovu <sipho.ndhlovu@outlook.com>
2021-01-29 14:50:58 +0200

    created Avatar component

---
+++ components/avatar.jsx
@@ -0,0 +1,14 @@
+import {Image} from 'rebass'
+
+export default function Avatar(props){
+  return (
+    <Image
+      {...props}
+      sx={{
+        width: [52, null, null, 80],
+        height: [52, null, null, 80],
+        borderRadius: '9999px'
+      }}
+    />
+  )
+}
