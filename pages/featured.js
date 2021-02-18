3453a0027527b34636af3b624532d7f0e6dadab2
Siphosenkosi Ndhlovu <sipho.ndhlovu@outlook.com>
2021-02-18 12:13:52 +0200

    Removed links from featured

--- pages/featured.js
+++ pages/featured.js
@@ -10,7 +10,10 @@ export default function Publication() {
   return (
     <Layout>
       <Head>
-        <meta name="description" content="Read about Kosta Du and Paymob Smart POS stories in the news and on the web " />
+        <meta
+          name="description"
+          content="Read about Kosta Du and Paymob Smart POS stories in the news and on the web "
+        />
         <title>Kosta Du and Paymob Smart POS in the news and on the web </title>
       </Head>
       <Container as="section" pt={9} display={[null, 'flex']}>
@@ -19,7 +22,7 @@ export default function Publication() {
             Kosta Du and Paymob in the news and on the web
           </Heading>
         </Main>
-        <Aside withAside width={[null, '40%']} ml={0} mb='-50px'>
+        <Aside withAside width={[null, '40%']} ml={0} mb="-50px">
           <Image src="/images/coffee.svg" alt="coffee cup" />
         </Aside>
       </Container>
@@ -33,29 +36,27 @@ export default function Publication() {
               gridTemplateColumns: ['1fr', '1fr 1fr'],
             }}
           >
-            {publications.map((publication) => {
-              if (publication.url) {
-                return (
-                  <Card as="a" href={publication.url} color="unset" sx={{ textDecoration: "unset"}}>
-                    <Image
-                      src={'/images/' + publication.logo + '.png'}
-                      mb={[6, null, null, null, 8]}
-                      height="42px"
-                    />
-                    <Heading
-                      as="h2"
-                      color="blue"
-                      fontSize={[6, null, null, 7]}
-                      mb={[6]}
-                    >
-                      {publication.headline}
-                    </Heading>
-                    <Text fontSize="4" opacity="0.3">
-                      {publication.summary}
-                    </Text>
-                  </Card>
-                )
-              }
+            {publications.map((publication, i) => {
+              return (
+                <Card key={i}>
+                  <Image
+                    src={'/images/' + publication.logo + '.png'}
+                    mb={[6, null, null, null, 8]}
+                    height="42px"
+                  />
+                  <Heading
+                    as="h2"
+                    color="blue"
+                    fontSize={[6, null, null, 7]}
+                    mb={[6]}
+                  >
+                    {publication.headline}
+                  </Heading>
+                  <Text fontSize="4" opacity="0.3">
+                    {publication.summary}
+                  </Text>
+                </Card>
+              );
             })}
           </Main>
         </Container>
